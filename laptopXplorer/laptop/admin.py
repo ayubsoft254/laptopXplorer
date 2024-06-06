24b6bf79bcd3c3cabba0282bac9daca1f0c768f2
Ayubsoft254 <henryoayub15@gmail.com>
2024-06-06 15:15:11 +0300

    update

--- laptopXplorer/laptop/admin.py
+++ laptopXplorer/laptop/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from laptop.models import Laptop
 
 # Register your models here.
+admin.site.register(Laptop)
--- laptopXplorer/laptopXplorer/urls.py
+++ laptopXplorer/laptopXplorer/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from laptop.views import site_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', site_home, name='sitehome' )
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
