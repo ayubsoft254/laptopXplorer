69e4e36ea4f4419ee6d3ce40623a37a5c5621d2c
Ayubsoft254 <henryoayub15@gmail.com>
2024-06-08 12:15:09 +0300

    navbar-update

--- laptopXplorer/laptop/views.py
+++ laptopXplorer/laptop/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from laptop.models import Laptop
 
 # Create your views here.
-def site_home(request):    
-    return render(request, 'index.html')
+def site_home(request):
+    laptops = Laptop.objects.all()
+    context = {'laptops':laptops}         
+    return render(request, 'index.html', context)
