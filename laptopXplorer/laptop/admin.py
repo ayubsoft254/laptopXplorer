f3c6b82ca18604259c3d1e3f33e31e662219ef07
Ayubsoft254 <henryoayub15@gmail.com>
2024-07-19 09:37:39 +0300

    Remove unnecessary files and code related to the laptop app

--- laptopXplorer/laptop/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from laptop.models import Laptop
-
-# Register your models here.
-admin.site.register(Laptop)
--- laptopXplorer/laptop/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from laptop.models import Laptop
-
-# Create your views here.
-def site_home(request):
-    laptops = Laptop.objects.all()
-    context = {'laptops':laptops}         
-    return render(request, 'index.html', context)
