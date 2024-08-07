158c2591732d00d82f7de589a8f39a65294e842a
Ayubsoft254 <henryoayub15@gmail.com>
2024-08-07 13:25:55 +0300

    feat: Update templates to include nav and footer components
    This commit updates the base.html template to include the nav and footer components. It also adds the necessary HTML files for the nav and footer components.

--- src/laptops/views.py
+++ src/laptops/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def landing_view(request):
-    return render(request, "landing.html", {})
+    return render(request, "land.html", {})
