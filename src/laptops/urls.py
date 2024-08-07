cdb3e6d3dff02885631c5984c5c634e564d49e7e
Ayubsoft254 <henryoayub15@gmail.com>
2024-08-07 14:53:57 +0300

    feat: Add laptop app, update URLs, and templates
    This commit adds the laptop app to the project, updates the URLs to include the laptop app's URLs, and includes the necessary changes to the base.html and land.html templates to support the new app.

--- src/laptops/urls.py
+++ src/laptops/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import landing_view
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import landing_view, laptops_list, laptop_detail
 
 urlpatterns = [
     path('', landing_view, name='landing_view'),
+    path('laptops', laptops_list, name='laptop_list' ),
+    path('laptops/<int:laptop_id>/', laptop_detail, name='laptop_detail'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/laptops/views.py
+++ src/laptops/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Laptop
 
 # Create your views here.
 def landing_view(request):
     return render(request, "land.html", {})
+
+def laptops_list(request):
+    laptops = Laptop.objects.all()
+    context = {
+        'laptops': laptops  # Use the variable instead of string
+    }
+    return render(request, "laptops/laptop_list.html", context)
+
+def laptop_detail(request, laptop_id):
+    laptop = get_object_or_404(Laptop, id=laptop_id)  # Use get_object_or_404 for better error handling
+    context = {
+        'laptop': laptop  # Use a singular form to represent a single laptop
+    }
+    return render(request, "laptops/laptop_detail.html", context)
