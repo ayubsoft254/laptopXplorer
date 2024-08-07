bec8107731cd2e385a86ef92a292358f9f9ae005
Ayubsoft254 <henryoayub15@gmail.com>
2024-08-07 13:30:16 +0300

    feat: Add pillow to requirements.txt and refactor Laptop model

--- src/laptops/models.py
+++ src/laptops/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Laptop(models.Model):
+    name = models.CharField(max_length=200)
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    specifications = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    review = models.TextField()
+    rating = models.FloatField()
+    image = models.ImageField(upload_to='laptop_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
