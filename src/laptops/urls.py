f5d15efc427248ae4243670e45bf70ab0a54c471
Ayubsoft254 <henryoayub15@gmail.com>
2024-08-09 12:56:30 +0300

    feat: Add laptops by brand functionality
    This commit adds the functionality to view laptops by brand in the laptop app. It includes the creation of the laptops_by_brand view in views.py, the addition of the corresponding URL pattern in urls.py, and the creation of the laptops_by_brand.html template.

--- src/laptops/urls.py
+++ src/laptops/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import landing_view, laptops_list, laptop_detail
+from .views import landing_view, laptops_list, laptop_detail, laptops_by_brand
 
 urlpatterns = [
     path('', landing_view, name='landing_view'),
     path('laptops', laptops_list, name='laptop_list'),
-    path('laptops/<int:laptop_id>/', laptop_detail, name='laptop_detail'),    
+    path('laptops/<int:laptop_id>/', laptop_detail, name='laptop_detail'),
+    path('brand/<int:brand_id>/', laptops_by_brand, name='laptops_by_brand'),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/laptops/views.py
+++ src/laptops/views.py
@@ -1,24 +1,37 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Laptop
+from .models import Laptop, Brand
 
 
 def landing_view(request):
     return render(request, "land.html", {})
 
 def laptops_list(request):
-    laptops = Laptop.objects.all()    
+    laptops = Laptop.objects.all()
+    latest = Laptop.objects.all().order_by('-created_at')[:4]
+    brands = Brand.objects.all()    
 
     context = {
-        'laptops': laptops
+        'laptops': laptops,
+        'latest':latest,
+        'brands':brands,
     }
     return render(request, "laptops/laptop_list.html", context)
 
 def laptop_detail(request, laptop_id):
     laptop = get_object_or_404(Laptop, id=laptop_id)
-    latest = Laptop.objects.all().order_by('-created_at')[:6]
+    latest = Laptop.objects.all().order_by('-created_at')[:4]
     context = {
         'laptop': laptop,
         'latest':latest,        
     }
     return render(request, "laptops/laptop_detail.html", context)
+
+def laptops_by_brand(request, brand_id):
+    brand = get_object_or_404(Brand, id=brand_id)
+    laptops = Laptop.objects.filter(brand=brand)
+    context = {
+        'brand': brand,
+        'laptops': laptops,
+    }
+    return render(request, "laptops/laptops_by_brand.html", context)
 
