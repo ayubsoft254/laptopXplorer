6180617b1012006ec9a6db7b52700b50e3b2720f
ayubsoft254 <henryoayub15@gmail.com>
2026-02-06 15:17:00 +0300

    Remove unused templates and components, including base layout, navigation, footer, and various laptop-related pages. Add .gitignore file to manage untracked files and directories.

--- src/laptops/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Laptop, Brand, Rating, Category
-
-# Register your models here.
-admin.site.register(Laptop)
-admin.site.register(Brand)
-admin.site.register(Rating)
-admin.site.register(Category)
